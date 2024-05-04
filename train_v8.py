301b9c10c590e9c6b076e859aaeaa22acac8e709
wyl516w <wyl21w@126.com>
2024-05-05 01:06:09 +0800

    yolov8 for dronevehicle, init from ultralytics

---
+++ train_v8.py
@@ -0,0 +1,33 @@
+import sys
+import argparse
+import os
+
+
+from ultralytics import YOLO
+
+def main(opt):
+    yaml = opt.cfg
+    model = YOLO(yaml) 
+
+    model.info()
+
+    results = model.train(data='vehicle.yaml',  # 训练参数均可以重新设置
+                        epochs=300, 
+                        imgsz=640, 
+                        workers=8, 
+                        batch=16,
+                        )
+
+def parse_opt(known=False):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default='yolov8.yaml', help='initial weights path')
+    parser.add_argument('--weights', type=str, default='', help='')
+
+    opt = parser.parse_known_args()[0] if known else parser.parse_args()
+    return opt
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
+
+    
